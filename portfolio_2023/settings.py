942d0a1d55ffecdb8f80dae65a27335f72912b37
Sam <montelongo.dev@gmail.com>
2023-06-01 20:51:09 -0500

    env var work

--- portfolio_2023/settings.py
+++ portfolio_2023/settings.py
@@ -18,8 +18,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', False)
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(' ')
-
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', ]
+# ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(',')
 
 # Application definition
 
